e295b66560d56aac8a25fc90df8cbf2e0b8aea80
Hanumanth <bodanapu94@gmail.com>
2021-04-29 13:40:55 +0530

    added comments

--- hangman.py
+++ hangman.py
@@ -1,13 +1,18 @@
 import sys
 import random
 from time import sleep
+# Hangman game with Words from file.
+# No of spaces are fetched dynamically according to word length.
+# features: admin and player.
+# used modules time,sys,random.
 
 name = input("UserName: ")
-welcome = "Hello, \"{n}\"".format(n=name)
-print(welcome)
+WELCOME = "Hello, \"{n}\"".format(n=name)
+print(WELCOME)
 
 
 def menu():
+    """Menu of game contains play and admin options"""
     print("--------------  MENU  ---------------")
     print("1: PLAYER")
     print("2: ADMIN")
@@ -26,6 +31,7 @@ def menu():
 
 
 def admin():
+    """ contains all the admin controls like adding words and viewing words that are used in game"""
     print("\t *** WELCOME ADMIN ***\t")
     print("MANAGE THE WORDS IN GAME")
     print("1: Add")
@@ -42,6 +48,10 @@ def admin():
 
 
 def add():
+    """
+    appending new word to words.txt file in a new line everytime
+    and using with for opening file so that file closes automatically
+    """
     new_word = input("Enter word to Add: ")
     with open('words.txt', "a") as file:
         file.write(new_word + "\n")
@@ -59,6 +69,9 @@ def add():
 
 
 def view():
+    """reading contents of words.txt file and using with for opening file
+    so that file closes automatically
+    """
     sub = []
     with open("words.txt", "r") as file:
         wordlist = set(file.readlines())
@@ -69,18 +82,26 @@ def view():
 
 
 def play():
+    """
+    play game every time it chooses a random word from words.txt file.
+    spaces will be generated according to the length of word.
+    total of 10 chances are given to guess the correct word.
+    used set while reading file data with read lines to take away duplicate word.
+    used string formatting. and sleep to provide some delay between function working.
+    """
     print(f"\"{name}\"  LETS\'S PLAY HANGMAN!")
     print(" You have 10 chances to guess the word\n")
     newest = []
     with open("words.txt", "r") as file:
-        wordlist = set(file.readlines())  # using set to take awy duplicates words that are in file more than once.
+        # using set to take awy duplicates words that are in file more than once.
+        wordlist = set(file.readlines())
         for i in wordlist:
-            newest.append(i.replace("\n", ""))
+            newest.append(i.replace("\n", ""))   # appending items of wordlist to newest list
 
     word = random.choice(newest)
     print("The word You have to guess is " + str(len(word)) + " characters long")
     guesses = " "
-    turn = 10
+    turn = 10     # number of chances given to guess the word.
     while turn > 0:
         wrong = 0
         for char in word:
@@ -105,7 +126,7 @@ def play():
         if guess not in word:
             turn -= 1
             print("\nOH NO! ... invalid character, ", end="")
-            print(f"You have {turn} chances left.")
+            print(f"You have {turn} chances left.")     # shows number of turns left
             if turn == 0:
                 print("You are HANGED!!")
                 choice = input("Play Again? y/n\n")
