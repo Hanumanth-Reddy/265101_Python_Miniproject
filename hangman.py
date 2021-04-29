b67a5e91399151b5a2ba397b9b47dbd6ade120f8
Hanumanth <bodanapu94@gmail.com>
2021-04-29 17:32:45 +0530

    added comments

--- hangman.py
+++ hangman.py
@@ -1,10 +1,7 @@
 import sys
 import random
 from time import sleep
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 # Hangman game with Words from file.
 # No of spaces are fetched dynamically according to word length.
 # features: admin and player.
@@ -57,11 +54,8 @@ def add():
     and using with for opening file so that file closes automatically
     """
     new_word = input("Enter word to Add: ")
-<<<<<<< HEAD
     new_word = new_word.lower()  # case conversion to avoid conflicts while play game
-    print(new_word)
-=======
->>>>>>> origin/master
+
     with open('words.txt', "a") as file:
         file.write(new_word + "\n")
         sleep(1)
@@ -99,30 +93,20 @@ def play():
     used string formatting. and sleep to provide some delay between function working.
     """
     print(f"\"{name}\"  LETS\'S PLAY HANGMAN!")
-<<<<<<< HEAD
     print(" You have 8 chances to guess the word\n")
-=======
+
     print(" You have 10 chances to guess the word\n")
->>>>>>> origin/master
     newest = []
     with open("words.txt", "r") as file:
         # using set to take awy duplicates words that are in file more than once.
         wordlist = set(file.readlines())
         for i in wordlist:
-<<<<<<< HEAD
             newest.append(i.replace("\n", ""))  # appending items of wordlist to newest list
-=======
-            newest.append(i.replace("\n", ""))   # appending items of wordlist to newest list
->>>>>>> origin/master
-
     word = random.choice(newest)
     print("The word You have to guess is " + str(len(word)) + " characters long")
     guesses = " "
-<<<<<<< HEAD
     turn = 8  # number of chances given to guess the word.
-=======
-    turn = 10     # number of chances given to guess the word.
->>>>>>> origin/master
+
     while turn > 0:
         wrong = 0
         for char in word:
@@ -133,7 +117,7 @@ def play():
                 wrong += 1
         if wrong == 0:
             sleep(1)
-<<<<<<< HEAD
+
             print("\nYAY You Are Saved!")
             try_again()
         guess = input("\tGuess Character: ")[0]  # reads only 1st character from input
@@ -161,34 +145,6 @@ def try_again():
     else:
         print("ERROR: Wrong Choice")
         sys.exit()
-=======
-            print("\nYAY YOUR Saved!")
-            choice = input("Play Again? y/n\n")
-            if "y" in choice:
-                play()
-            elif "n" in choice:
-                print("THANK YOU FOR PLAYING")
-                sys.exit()
-            else:
-                print("ERROR: Wrong Choice")
-                sys.exit()
-        guess = input("\tGuess Character: ")[0]  # reads only 1st character from input
-        if guess not in word:
-            turn -= 1
-            print("\nOH NO! ... invalid character, ", end="")
-            print(f"You have {turn} chances left.")     # shows number of turns remaining
-            if turn == 0:
-                print("You are HANGED!!")
-                choice = input("Play Again? y/n\n")
-                if "y" in choice:
-                    play()
-                elif "n" in choice:
-                    print("Thank You!")
-                    sys.exit()
-                else:
-                    print("ERROR: Wrong Choice")
-                    sys.exit()
->>>>>>> origin/master
 
 
 menu()
